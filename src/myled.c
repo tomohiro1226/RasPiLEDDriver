35b869961aa3ac5a38861f71df863915a51cdc38
tomohiro <12.26tomo.aoki@gmail.com>
2018-01-30 19:28:44 +0900

    fix: ファイルを分けることに失敗したので結合

--- src/myled.c
+++ src/myled.c
@@ -14,10 +14,51 @@
 #include <linux/string.h>
 #include <linux/delay.h>
 
-#include <myGpio.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/io.h>
+
+#define BASEADD 0x3F200000 // rpi2/3
+// #define BASEADD 0x2000000 // rpi1
+
+#define MAPBLOCK 4096 
+
+/* 
+* GPFSEL0 GPIO0～GPIO9のFSEL
+* GPFSEL1 GPIO10～GPIO19のFSEL
+* GPFSEL2 GPIO20～GPIO29のFSEL
+* GPFSEL3 GPIO30～GPIO39のFSEL 
+*/
+#define GPFSEL0 *((volatile uint32_t*)(gpio_map+0x00))
+#define GPFSEL1 *((volatile uint32_t*)(gpio_map+0x04))
+#define GPFSEL2 *((volatile uint32_t*)(gpio_map+0x08))
+#define GPFSEL3 *((volatile uint32_t*)(gpio_map+0x0C))
+
+#define GPIOH *((volatile uint32_t*)(gpio_map+0x1c))
+#define GPIOL *((volatile uint32_t*)(gpio_map+0x28))
+
+#define GPIO1 0x01
+#define GPIO2 0x02
+#define GPIO3 0x03
+#define GPIO4 0x04
+#define GPIO5 0x05
+#define GPIO6 0x06
+#define GPIO7 0x07
+#define GPIO8 0x08
+#define GPIO9 0x09
+#define GPIOA 0x0A
+#define GPIOB 0x0B
+#define GPIOC 0x0C
+#define GPIOD 0x0D
+#define GPIOE 0x0E
+#define GPIOF 0x0F
+
+
+static void __iomem *gpio_map; //仮想アドレスと物理アドレスのマッピング
 
 /*ドライバ名*/
-#define MYLED_DEVNAME   "myled"
+#define MYLED_DEVNAME "myled"
 /*ドライバのメジャー番号*/
 #define MYLED_MAJOR     201
 
@@ -31,6 +72,12 @@ static int myled_open(struct inode *inode, struct file *filp);
 static int myled_close(struct inode *inode, struct file *filp);
 static ssize_t myled_write(struct file *filp, const char *buf, size_t count, loff_t *f_pos);
 static ssize_t myled_read(struct file *filp, char *buf, size_t count, loff_t *f_pos);
+
+int gpioInit(void);
+int gpioExit(void);
+int gpioMode(uint32_t b, uint32_t s);
+int gpioSet(uint32_t g);
+int gpioClear(uint32_t g);
 
 
 /*グローバル変数の宣言*/
@@ -50,14 +97,7 @@ static char *myled_devname = MYLED_DEVNAME;
 /*カーネルモジュールロード時*/
 static int myled_init(void){
 
-	// ledドライバの設定
-	gpioInit();
-
-	// 2-4を出力設定
-	#define GPIOMODE 011100
-	// 3色LEDの設定
-	gpioMode(GPFSEL0, GPIOMODE);
-
+	
 	/*デバイスドライバメジャー番号の登録*/
 	if( register_chrdev(myled_major, myled_devname , &myled_fops) < 0 ){
 		printk(KERN_ERR "myled error: register_chrdev\n");
@@ -65,6 +105,13 @@ static int myled_init(void){
 	
 	printk(KERN_INFO "%s loaded. major:%0x \n", myled_devname, myled_major);
 
+	// ledドライバの設定
+	gpioInit();
+
+	// 2-4を出力設定
+	#define GPIOMODE 011100
+	// 3色LEDの設定
+	GPFSEL0 = GPIOMODE;
 
 	// 実験
 	gpioSet(GPIO4);
@@ -79,7 +126,6 @@ static int myled_init(void){
 	msleep(1000);
 	gpioClear(GPIO4);
 	msleep(1000);
-	
 	
 	return 0;
 }
@@ -145,4 +191,45 @@ static ssize_t myled_read(struct file *filp, char *buf, size_t count, loff_t *f_
 module_init(myled_init);
 module_exit(myled_exit);
 
+int gpioInit(void){
 
+    printk(KERN_INFO "myGpio init\n");
+
+    gpio_map = NULL;
+
+    // 仮想アドレスと物理アドレスのマッピング
+    gpio_map = ioremap_nocache(BASEADD, MAPBLOCK);
+
+    return (gpio_map!=NULL) ? 0 : 1; 
+}
+
+/** 
+ * GPIOポートをHighに
+ * @param g ポート番号(GPIO*)
+*/
+int gpioSet(uint32_t g){
+    GPIOH = g;
+
+    return (GPIOH==g) ? 0 : 1;
+}
+
+/** 
+ * GPIOポートをLowに
+ * @param g ポート番号(GPIO*)
+*/
+int gpioClear(uint32_t g){
+    GPIOL = g;
+
+    return (GPIOH==g) ? 0 : 1;
+}
+
+int gpioExit(void){
+
+    
+    printk(KERN_INFO "myGpio exit\n");
+
+    // マッピングの解放
+    iounmap(gpio_map);
+
+    return 0;
+}
