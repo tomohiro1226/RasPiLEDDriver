867498a5ef60ecdf7530c8d2ba33c07d7e26d4bb
Tomohiro <12.26tomo.aoki@gmail.com>
2018-01-31 10:44:44 +0900

    fix: userPGの修正

--- test/userPG.c
+++ test/userPG.c
@@ -6,13 +6,21 @@
 int main(void){
 
     int fd;
+    char str[10];
 
     fd = fileOpen("/dev/myled", _WRITE);
     if(fd < 0){
         perror("cannot open");
         exit(EXIT_FAILURE);
     }
-    fileWrite(fd, "HG");
+//    fileWrite(fd, "HG");
+    while(1){
+
+        scanf("%s",str);
+        if(strcmp(str, "END") == 0)break;
+        fileWrite(fd, str);
+    }
+    
     fileClose(fd);
 
     return 0;
